05b172e579ab019b0e7b4c7c33886ce3c30ede21
Flavio Augusto <ra197083@correa.lab.ic.unicamp.br>
2019-08-16 20:46:03 -0300

    LAB01

---
+++ py2c/cebolinha.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_L 10
+
+int main() {
+    char palavra[MAX_L];
+ 
+    scanf("%s", palavra);
+    printf("Você escreveu %s\n", palavra);
+    return 0;
+}
