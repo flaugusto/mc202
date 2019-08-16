598a7aa85a8b3c914ab57564925382d9058dc843
Flavio Augusto <ra197083@paralamas.lab.ic.unicamp.br>
2019-08-15 21:53:11 -0300

    Minicalculadora completa

--- py2c/calc.c
+++ py2c/calc.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 
 int main() {
+    int nop, i;
     float a, b, res;
     char op;
 
-    scanf("%f %c %f", &a, &op, &b);
-
-    if (op == '/') {
-        res = a / b;
-        printf("%f\n", res);
-    } else {
-        printf("Erro: operador não reconhecido.\n");
+    scanf("%d", &nop);
+    
+    for (i = 0; i < nop; i++) {
+        scanf("%f %c %f", &a, &op, &b);
+        switch (op) {
+            case '/':
+                res = a / b;
+                printf("%f\n", res);
+                break;
+            case '+':
+                res = a + b;
+                printf("%f\n", res);
+                break;
+            case '*':
+                res = a * b;
+                printf("%f\n", res);
+                break;
+            case '-':
+                res = a - b;
+                printf("%f\n", res);
+                break;
+            default:
+                printf("Erro: operador não reconhecido.\n");
+                break;
+        }
     }
+    
+    
     return 0;
 }
