c42449cebc3103b6a944709b21ef488fc75e6ff9
Flavio Augusto <ra197083@correa.lab.ic.unicamp.br>
2019-08-16 22:42:37 -0300

    WIP Lab01

--- lab01/lab01.c
+++ lab01/lab01.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 1000
+
+void readArr(double arr[], int arrlen) 
+{
+   int i;
+   for (i = 0; i < arrlen; i++)
+   {
+      scanf("%lf", &arr[i]);
+   }
+}
+
+void genSnail(double arr[MAX][MAX], int m, int n)
+{
+   
+}
 
 int main()
 {
+   int m, n;
+   scanf("%d %d", &m, &n);
+
+   int len = m * n; 
+   double nums[len];
+   readArr(nums, len);
    
-   
-   
+   /*
+   double snail[len][len];
+   genSnail(nums, snail);
+
+   printMatrix(snail); */
+
    return EXIT_SUCCESS;
 }
