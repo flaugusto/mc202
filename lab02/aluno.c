41aa3f94ca4bb92483b918b2c535a19733e7f448
Flavio Augusto <augustof.cunha@outlook.com>
2019-08-27 20:15:31 -0300

    lab02 first commit

---
+++ lab02/aluno.c
@@ -0,0 +1,29 @@
+#include "aluno.h"
+
+Aluno criar_aluno(int registro_academico, float desempenho)
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
+
+Aluno adicionar_disciplina(Aluno aluno, const char disciplina[])
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
+
+float obter_desempenho(Aluno aluno)
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
+
+bool validar_matricula(Aluno aluno, const char disciplina[])
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
---
+++ lab02/professor.c
@@ -0,0 +1,35 @@
+#include "professor.h"
+
+Professor criar_professor(const char nome[], float salario, const char disciplina[])
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
+
+Professor aplicar_aumento(Professor professor, float porcentagem)
+{
+   professor.salario *= (1.0+porcentagem); //  Obs.: Não modifique esta função!
+   return professor;
+}
+
+void obter_nome(Professor professor, char nome[])
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
+
+float obter_salario(Professor professor)
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
+
+void obter_disciplina(Professor professor, char disciplina[])
+{
+   /** ************************************************************************
+    *               ATENÇÃO: É preciso implementar esta função!               *
+    *********************************************************************** **/
+}
