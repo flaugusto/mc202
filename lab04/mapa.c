48dd940fd8badaf8702b2eabdff5e7c4adf1b217
Flavio Augusto <augustof.cunha@outlook.com>
2019-09-17 22:50:04 -0300

    LAB04 Final

--- lab04/mapa.c
+++ lab04/mapa.c
@@ -31,11 +31,14 @@ void imprimir_mapa(char** mapa, int m) {
    }
 }
 
-void limpar_mapa(char** mapa, int m, int n, char m_esp, char m_min) {
+void limpar_mapa(char** mapa, int m, int n, char m_esp, char m_min, char m_parcial) {
     for (int i = 1; i < m - 1; i++)
         for (int j = 1; j < n - 1; j++)
             if (mapa[i][j] == m_esp || mapa[i][j] == m_min)
                 mapa[i][j] = ' ';
+            else if (mapa[i][j] == m_parcial) {
+                mapa[i][j] = SOL;
+            }
 }
 
 void desalocar_mapa(char** mapa, int m){
--- lab04/mapa.h
+++ lab04/mapa.h
@@ -1,3 +1,12 @@
+#define M_MIN        ',' // Marcador do caminho p/ o minotauro
+#define M_ESP        '.' //    ''         ''       a espada
+#define ESPADA       'S' // Char que representa a espada
+#define MINOTAURO    'M' // ''      ''          o minotauro
+#define ENT          'E' // ''      ''          a entrada
+#define PAREDE       '#' // ''      ''          a parede
+#define SOL          '*' // ''      ''          o caminho solução
+#define SOL_P        '/' // ''      ''          o caminho solução parcial
+
 /** Cria a matriz do labirinto e retorna a matriz alocada
  * m -> numero de linhas
  * n -> numero de colunas
@@ -23,8 +32,9 @@ void imprimir_mapa(char** mapa, int m);
  * n -> colunas
  * m_esp -> marcador do camindo da espada 
  * m_min -> marcador do caminho do minotauro 
+ * m_parcial -> marcador de uma solução parcial
  * */
-void limpar_mapa(char** mapa, int m, int n, char m_esp, char m_min);
+void limpar_mapa(char** mapa, int m, int n, char m_esp, char m_min, char m_parcial);
 
 /** Desaloca a memória anteriormente alocada para o labirinto
  * mapa -> labirinto alocado
