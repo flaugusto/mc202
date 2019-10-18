af5906d1784457d9194d0cf573021296c60f5fb9
Flavio Augusto <flavio.augusto@icarotech.com>
2019-10-18 20:23:47 -0300

    LAB07: Leitura das expressões

--- lab07/lab07.c
+++ lab07/lab07.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "arvore.h"
 
-p_node fill_tree(Data v) {
+// p_node fill_tree(Data v) {
 
-}
+// }
 
 int main() {
-   int m;
-   char* buffer;
+   int m, num;
+   char buffer[100];
    scanf("%d", &m);
-
+   
    for (int i = 0; i < m; i++)
    {
-      scanf(" %s", &buffer);
+      do {
+         scanf(" %s", buffer);
+         if (buffer[0] == '(') {
+            //Inicio de expressão
+
+         }
+         else if (buffer[0] >= '0' && buffer[0] <= '9') {
+            // Número
+            num = atoi(buffer);
+         }
+         else  {
+            // Incógnita
+         }
+      } while (getchar() != '\n');
    }
    
-
-
-
+   
+      
+   
    return EXIT_SUCCESS;
 }
