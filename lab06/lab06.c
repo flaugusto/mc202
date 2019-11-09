0dcf574960789c61d4e9c8820a51bae07181224b
Flavio Augusto <augustof.cunha@outlook.com>
2019-11-09 18:38:39 -0300

    LAB06: WIP

--- lab06/lab06.c
+++ lab06/lab06.c
@@ -8,6 +8,7 @@
 
 void print_stack(p_stack stack);
 void print_list(p_node head);
+int to_int(char c);
 
 int main() {
     int lines, count, x;
@@ -17,18 +18,18 @@ int main() {
     for (int i = 0; i < lines; i++) {
         // Início de uma sequência
         char seq[MAX_SIZE];
-        int len = strlen(seq);
-        p_stack result = create_stack();
         scanf("%s",seq);
         scanf("%d", &count);
         printf("Dig: %d\n", count);
+
+        int len = strlen(seq);
+        p_stack result = create_stack();
         
 
         int j = 0;
         for (; j < len && count != 0; j++) {
-            char c = seq[j];
-            x = c - '0'; // pega o valor inteiro
-            // printf("%d\n", x);
+            x = to_int(seq[j]);
+            printf("%d\n", x);
 
             if (empty(result)) {
                 push(x, result);
@@ -39,24 +40,22 @@ int main() {
                 count--;
             }
             push(x, result);
+            printf("Stack: ");
+            print_stack(result);
         }
 
         if (count != 0) {
             x = pop(result);
             count--;
-            while (count != 0 && x < peek(result)) {
+            while (count != 0 && x <= peek(result)) {
                 x = pop(result);
                 count--;
             }
         }
 
-        if (j < len) {
-            for (int k = 0; k < len; k++) {
-                char c = seq[j];
-                x = c - '0'; // pega o valor inteiro
-                push(x, result);
-            }
-            
+        // Se ainda sobrou números na sequencia, adiciona na pilha
+        for (; j < len; j++) {
+            push(to_int(seq[j]), result);
         }
 
         print_stack(result);
@@ -68,8 +67,10 @@ int main() {
 }
 
 void print_stack(p_stack stack) {
-    if (!empty(stack))
+    if (!empty(stack)) {
         print_list(get_top(stack));
+        printf("\n");
+    }
 }
 
 void print_list(p_node head) {
@@ -77,4 +78,8 @@ void print_list(p_node head) {
         print_list(get_next(head));
         printf("%d", get_value(head));
     }
+}
+
+int to_int(char c) {
+    return c - '0';
 }
